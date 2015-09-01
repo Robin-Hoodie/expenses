2f07e511346d6911c4010ec50ec0de0a1c5fde46
RobinHellemans <Robin.Hellemans@dhl.com>
2015-09-01 10:45:16 +0200

    Added some temporary files to set up directory structure
    Set up some temporary files to set up directory structure.
    Also added maven support.

---
+++ src/main/java/com/expenses/temp/Bar.java
@@ -0,0 +1,7 @@
+package com.expenses.temp;
+
+/**
+ * Created by rohellem on 01/09/2015.
+ */
+public class Bar {
+}
---
+++ src/test/java/com/expenses/test/Foo.java
@@ -0,0 +1,7 @@
+package com.expenses.test;
+
+/**
+ * Created by rohellem on 01/09/2015.
+ */
+public class Foo {
+}
