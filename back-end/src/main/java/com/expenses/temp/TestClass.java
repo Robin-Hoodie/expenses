b0080d989a9c02daf5bfe6e8253638372eb7e417
RobinHellemans <Robin.Hellemans@dhl.com>
2015-09-01 12:36:49 +0200

    Set up back-end and front-end module with basic project skeleton

---
+++ back-end/src/main/java/com/expenses/temp/TestClass.java
@@ -0,0 +1,11 @@
+package com.expenses.temp;
+
+/**
+ * Created by rohellem on 01/09/2015.
+ */
+public class TestClass {
+
+	public static int onePlusOne() {
+		return 1 + 1;
+	}
+}
---
+++ back-end/src/test/java/com/expenses/test/TestClassTest.java
@@ -0,0 +1,17 @@
+package com.expenses.test;
+
+import com.expenses.temp.TestClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rohellem on 01/09/2015.
+ */
+public class TestClassTest {
+
+	@Test
+	public void testOnePlusOne() {
+		assertEquals(2, TestClass.onePlusOne());
+	}
+}
