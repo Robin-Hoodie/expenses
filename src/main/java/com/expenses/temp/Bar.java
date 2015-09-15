d45b118a1aa404de6c1a8b7a6aa26289477486ca
Robin Hellemans <robin.hellemans@realdolmen.com>
2015-09-15 17:13:09 +0200

    Deleted src folder in root

--- src/main/java/com/expenses/temp/Bar.java
+++
@@ -1,7 +0,0 @@
-package com.expenses.temp;
-
-/**
- * Created by rohellem on 01/09/2015.
- */
-public class Bar {
-}
--- src/test/java/com/expenses/test/Foo.java
+++
@@ -1,7 +0,0 @@
-package com.expenses.test;
-
-/**
- * Created by rohellem on 01/09/2015.
- */
-public class Foo {
-}
